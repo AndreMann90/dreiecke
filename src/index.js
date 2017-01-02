b88a6c59f5fcdc00022b56c323630ea941006ea1
AndreMann90 <Andre.Mann@rwth-aachen.de>
2017-01-02 14:12:59 +0100

    Added logging middleware

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware  } from 'redux'
+import createLogger from 'redux-logger';
 import measurementApp from './reducers'
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import './PrintStyle.css'
 
+let withStateDiff = false;
+let logger = createLogger({diff: withStateDiff});
 
-let store = createStore(measurementApp);
-
-console.log(store.getState());
+let store = createStore(
+    measurementApp,
+    applyMiddleware(logger)
+);
 
 render(
     <Provider store={store}>
