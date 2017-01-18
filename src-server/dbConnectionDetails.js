758d43c9379c6355bdd6ea3db9e4bddde4eb22a5
AndreMann90 <Andre.Mann@rwth-aachen.de>
2017-01-18 16:15:22 +0100

    Reading from DB. TODO: writing to DB; overview view for creating new "Baustellenaufaße", opening/deleting existing once

---
+++ src-server/dbConnectionDetails.js
@@ -0,0 +1,9 @@
+
+// PostgreSQL connection details
+export const dbConnection = {
+    host: 'localhost',
+    port: 5433,
+    database: 'my-database-name',
+    user: 'user-name',
+    password: 'user-password'
+};
--- src-server/server.js
+++ src-server/server.js
@@ -3,6 +3,9 @@ const path = require('path');
 const fs = require('fs');
 const cheerio = require('cheerio');
 
+const pgp = require('pg-promise')();
+import dbConnection from './dbConnectionDetails';
+
 import React from 'react'
 import { renderToString } from 'react-dom/server'
 import { createStore } from 'redux'
@@ -14,27 +17,43 @@ import rootReducer from '../src/redux/index'
 const app = express();
 const port = 9000;
 
-app.get('/', function (req, res) {
-    let $ = cheerio.load(fs.readFileSync(path.join(__dirname, '../build', 'index.html')));
+const htmlTemplate = fs.readFileSync(path.join(__dirname, '../build', 'index.html')); // load template into memory
+const db = pgp(dbConnection);
 
-    // load the initial state
-    let preLoadedState = {present: {name: 'test'}};
+app.get('/', function (req, res) {
+
+    db.one("select * from states where id=$1", ['TestID'])
+        .then(data => {
+            const html = prepareHtml(data);
+            res.send(html);
+        })
+        .catch(error => {
+            const html = prepareHtml('');
+            res.send(html);
+        });
+});
+
+function prepareHtml(preLoadedStateString) {
+    preLoadedStateString = preLoadedStateString.replace(/</g, '\\x3c');
+    let $ = cheerio.load(htmlTemplate);
 
     // put the initial state into the html
-    let initScript = $(`<script>window.__PRELOADED_STATE__ = ${JSON.stringify(preLoadedState).replace(/</g, '\\x3c')}</script>`);
+    let initScript = $(`<script>window.__PRELOADED_STATE__ = ${preLoadedStateString}</script>`);
     $('#initState').replaceWith(initScript);
 
     // render the initial html
+    const preLoadedState = JSON.parse(preLoadedStateString);
     const store = createStore(rootReducer, preLoadedState);
     const startHtml = renderToString(
         <Provider store={store}>
             <App/>
         </Provider>
     );
+    // and put it into the html
     $('#app').append(startHtml);
 
-    res.send($.html());
-});
+    return $.html();
+}
 
 app.use(express.static('./build'));
 
