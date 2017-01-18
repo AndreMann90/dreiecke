db9131175712a89c1f01b68734a7282a2412b1bb
AndreMann90 <Andre.Mann@rwth-aachen.de>
2017-01-18 13:06:27 +0100

    Universal Javascript App

--- src-server/server.js
+++ src-server/server.js
@@ -2,18 +2,20 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const cheerio = require('cheerio');
-/* TODO make following imports work
+
+import React from 'react'
 import { renderToString } from 'react-dom/server'
 import { createStore } from 'redux'
-import App from './App';
-import { Provider } from 'react-redux'
-*/
+import App from '../src/App';
+import { Provider } from 'react-redux';
+import rootReducer from '../src/redux/index'
+
 
 const app = express();
 const port = 9000;
 
 app.get('/', function (req, res) {
-    let $ = cheerio.load(fs.readFileSync(path.join(__dirname, './build', 'index.html')));
+    let $ = cheerio.load(fs.readFileSync(path.join(__dirname, '../build', 'index.html')));
 
     // load the initial state
     let preLoadedState = {present: {name: 'test'}};
@@ -22,14 +24,14 @@ app.get('/', function (req, res) {
     let initScript = $(`<script>window.__PRELOADED_STATE__ = ${JSON.stringify(preLoadedState).replace(/</g, '\\x3c')}</script>`);
     $('#initState').replaceWith(initScript);
 
-    // render the initial html TODO see imports
-    /*const store = createStore(counterApp, preLoadedState);
-    const html = renderToString(
+    // render the initial html
+    const store = createStore(rootReducer, preLoadedState);
+    const startHtml = renderToString(
         <Provider store={store}>
             <App/>
         </Provider>
     );
-     $('#app').append(initScript); */
+    $('#app').append(startHtml);
 
     res.send($.html());
 });
